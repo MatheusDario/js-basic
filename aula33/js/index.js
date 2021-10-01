c7581953aa671e0c4a6e3c207b19b434b956013b
MatheusDario <dario.matheus97@gmail.com>
2021-10-01 13:47:38 -0300

    nodelist

--- aula33/js/index.js
+++ aula33/js/index.js
@@ -1,14 +1,25 @@
+//for classico
 for(let i = 0; i <= 10; i++) {
     const par = i % 2 === 0;
     //console.log(i, par)  
 }
-
 const frutas = ['maca', 'uva', 'abacaxi'];
 for (let i in frutas) {
-    //console.log(frutas[i])
+   // console.log(frutas[i])
 }
 
+//for in p/ obj
 const pessoa = { nome: 'Matheus', sobrenome: 'Dario', idade: 23};
 for (let dados in pessoa) {
-    console.log(dados ,pessoa[dados]);
+    //console.log(dados ,pessoa[dados]);
 }
+
+//for of p/ retornar os valores
+const nomes = ['Matheus', 'Arthur', 'Bianca']
+for(let valor of nomes) {
+    //console.log(valor)
+}
+
+nomes.forEach(function(valor, indice){
+    //console.log(valor, indice)
+})
---
+++ aula36/assets/js/main.js
@@ -0,0 +1,19 @@
+
+    //Colocar a cor de fundo nos paragrafos da mesma cor de fundo do background
+    //Colocar a cor do texto do paragrafo em branco 
+    const paragrafos = document.querySelector('.paragrafos');
+    const container = document.querySelector('.container')
+    const ps = paragrafos.querySelectorAll('p');
+    
+    const estilosBody = getComputedStyle(document.body);
+    const backGroundColorBody = estilosBody.backgroundColor;
+    
+
+   for (let p of ps) {
+       p.style.backgroundColor = backGroundColorBody;
+       p.style.color = '#FFFFFF';
+   }
+
+   container.innerHTML += `<p>reiciendis quaerat sed numquam totam perferendis, natus repellat ipsam, fugiat</p>`;
+
+   
