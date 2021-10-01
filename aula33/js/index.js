ee67bce9eacf2e80e38752149e2608e312137b60
MatheusDario <dario.matheus97@gmail.com>
2021-10-01 12:59:04 -0300

    for in

--- aula33/js/index.js
+++ aula33/js/index.js
@@ -1,4 +1,14 @@
 for(let i = 0; i <= 10; i++) {
     const par = i % 2 === 0;
-    console.log(i, par)  
+    //console.log(i, par)  
+}
+
+const frutas = ['maca', 'uva', 'abacaxi'];
+for (let i in frutas) {
+    //console.log(frutas[i])
+}
+
+const pessoa = { nome: 'Matheus', sobrenome: 'Dario', idade: 23};
+for (let dados in pessoa) {
+    console.log(dados ,pessoa[dados]);
 }
