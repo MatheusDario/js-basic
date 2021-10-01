5a6c6a0021934266b82a9722f3c613cc3d608518
MatheusDario <dario.matheus97@gmail.com>
2021-10-01 12:27:45 -0300

    exericicios for - comentarios

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -8,11 +8,11 @@ const elementos = [
 const container = document.querySelector('.container'); //cria a vari dentro da classe selecionada esse e o lugar espe
 const div = document.createElement('div'); // cria o elemento dentro do container usando appenchild
 
-for (let i = 0; i<elementos.length; i++) {
-    const {tag, texto} = elementos[i];
-    let tagCriada = document.createElement(tag)
-    tagCriada.innerText = texto;
-    div.appendChild(tagCriada);
+for (let i = 0; i<elementos.length; i++) { // vai percorrer o array de objetos
+    const {tag, texto} = elementos[i]; // desestrutura o obj e criar uma vari
+    let tagCriada = document.createElement(tag) // cria o elemento html
+    tagCriada.innerText = texto; // add o conteudo dentro da tag
+    div.appendChild(tagCriada); // add dentro da div a tag com o conteudo
 }
 
-container.appendChild(div); //cria a div no lugar especifico
+container.appendChild(div); //cria a div no lugar especifico no caso no container selecionado 
