00514a5cd3abd65078ce54f18cf63f02682ff575
MatheusDario <dario.matheus97@gmail.com>
2021-10-04 17:52:39 -0300

    lista de tarefas concluida

--- aula47-lista_tarefas/assets/js/main.js
+++ aula47-lista_tarefas/assets/js/main.js
@@ -3,9 +3,76 @@ function main() {
     const btnTarefa = document.querySelector('.btn-tarefa');
     const tarefas = document.querySelector('.tarefas');
 
+    function limpaInput() {
+        inputTarefa.value = '';
+        inputTarefa.focus();
+    }
+
+    function criaBtnApagar(li) {
+        li.innerHTML += ' ';
+        const btnApagar = document.createElement('button');
+        btnApagar.innerText = 'Apagar';
+        btnApagar.setAttribute('class', 'apagar');
+        li.appendChild(btnApagar);
+    }
+
+    function criaLi() {
+        const li = document.createElement('li');
+        return li;
+    }
+
+    function criaTarefa(textoInput) {
+       const li = criaLi();
+       li.innerHTML = textoInput;
+       tarefas.appendChild(li);
+       limpaInput();
+       criaBtnApagar(li);
+       salvarTarefas();
+    }
+
+    inputTarefa.addEventListener('keypress', function(e) {
+        if(e.keyCode === 13) {
+            if(!inputTarefa.value) return;
+            criaTarefa(inputTarefa.value);
+        }
+    })
+
+    //recuperando o evento de click do btn e as informacoes inseridas no input
     btnTarefa.addEventListener('click', function(){
         if(!inputTarefa.value) return;
-        console.log(inputTarefa.value);
-    })
+        criaTarefa(inputTarefa.value);
+    });
+
+    document.addEventListener('click', function(e){
+        const el = e.target;
+        if(el.classList.contains('apagar')) {
+            el.parentElement.remove();
+            salvarTarefas();
+        }
+    });
+
+    function salvarTarefas() {
+        const liTarefas = tarefas.querySelectorAll('li');
+        const listaDeTarefas = [];
+
+        for (let tarefa of liTarefas) {
+            let tarefaTexto = tarefa.innerText;
+            tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+            listaDeTarefas.push(tarefaTexto);
+        }
+        
+        const tarefasJSON = JSON.stringify(listaDeTarefas);
+        localStorage.setItem('tarefas', tarefasJSON);
+    }
+
+    function addTarefasSalvas() {
+        const tarefas = localStorage.getItem('tarefas');
+        const listaDeTarefas = JSON.parse(tarefas);
+        
+        for(let tarefa of listaDeTarefas) {
+            criaTarefa(tarefa);
+        }
+    }
+    addTarefasSalvas();
 }   
 main();
