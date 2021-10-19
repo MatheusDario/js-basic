65db6dfeb3018afb7e1425022b70d0bbf2107d8b
MatheusDario <dario.matheus97@gmail.com>
2021-10-19 11:56:14 -0300

    validador-cpf com class concluido

--- aula78-conta/cpf-com-class.js
+++ aula78-conta/cpf-com-class.js
@@ -1,5 +1,52 @@
-class validaCpf {
+class ValidaCpf {
     constructor(cpfEnviado) {
-        
+        Object.defineProperty(this, 'cpfLimpo', {
+            enumerable: true,
+            writable: false,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
     }
+
+    valida() {
+        if(typeof this.cpfLimpo !== 'string') return false;
+        if(!this.cpfLimpo) return false;
+        if(this.cpfLimpo.length !== 11) return false;
+        if(this.isSequencia()) return false;
+        this.geraNovoCpf();
+        return this.novoCpf === this.cpfLimpo;
+
+    };
+
+    geraNovoCpf() {
+        const cpfSemDigito = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCpf.geraDigito(cpfSemDigito);
+        const digito2 = ValidaCpf.geraDigito(cpfSemDigito + digito1);
+        this.novoCpf = cpfSemDigito + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigito) {
+        let total = 0;
+        let recursivo = cpfSemDigito.length + 1;
+        for(let stringNumerica of cpfSemDigito) {
+            total += recursivo * Number(stringNumerica);
+            recursivo --;
+        }
+        
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? digito : '0';
+    } 
+
+    isSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    };
 }
+
+const cpf = new ValidaCpf('028.129.931-54'); 
+if(cpf.valida()) {
+    console.log(`CPF Valido ` + cpf.cpfLimpo);
+} else {
+    console.log('CPF Invalido');
+}
+
+//console.log(cpf)
