7d6dda6d41d840a8a2fb96748cb370dcd18d657f
MatheusDario <dario.matheus97@gmail.com>
2021-10-18 22:21:31 -0300

    conta-com-class

--- aula78-conta/class.js
+++ aula78-conta/class.js
@@ -24,7 +24,36 @@ class Conta {
         this.extrato();
     }
 }
-const conta1 = new Conta(2219, 25410-2, 100);
-conta1.depositar(10)
-conta1.sacar(50)
-conta1.sacar(70)
+
+class ContaCorrente extends Conta {
+    constructor(agencia, conta, saldo, limite) {
+        super(agencia, conta, saldo);
+        this.limite = limite;
+    }
+
+    limite(valor) {
+        this.limite -= valor;
+        console.log(`O seu limite e: ${this.limite}`)
+    }
+
+    sacar(valor) {
+        if(valor > (this.saldo + this.limite)) {
+            console.log(`Saldo insuficiente: ${this.saldo}`);
+            return;
+        }
+        this.saldo -= valor;
+        console.log(`O seu saldo e: ${this.saldo} e seu limite e ${this.limite}`)
+    }
+
+    extrato() {
+        console.log(`Ag/C: ${this.agencia} ${this.conta} o seu extrato e: ${this.saldo} reais o seu limite e: R$${this.limite}`);
+    }
+    
+}
+
+const cc1 = new ContaCorrente(2249,82142, 100, 200);
+cc1.depositar(10);
+cc1.sacar(90);
+cc1.sacar(40);
+cc1.sacar(80);
+
