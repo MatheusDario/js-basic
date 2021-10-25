e7fa74958a305191b90002f3dc8450a7637ec1af
MatheusDario <dario.matheus97@gmail.com>
2021-10-25 17:22:23 -0300

    about axios

---
+++ aula91-axios/assets/js/main.js
@@ -0,0 +1,34 @@
+/*
+fetch('pessoas.json')
+.then(resposta => resposta.json())
+.then(json => carregaElementosNaPagina(json));
+*/
+
+axios('pessoas.json').then(resposta => carregaElementosNaPagina(resposta.data));
+
+function carregaElementosNaPagina(json) {
+    const table = document.createElement('table');
+    
+    for(let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        let td = document.createElement('td');
+        td.innerHTML = pessoa.nome;
+        tr.appendChild(td);
+
+        let td1 = document.createElement('td');
+        td1.innerHTML = pessoa.idade;
+        tr.appendChild(td1)
+
+        let td2 = document.createElement('td');
+        td2.innerHTML = pessoa.salario;
+        tr.appendChild(td2)
+
+        table.appendChild(tr);
+
+        
+    }
+    const resultado = document.querySelector('.resultado');
+    resultado.appendChild(table);
+
+};
